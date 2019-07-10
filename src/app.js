fe28c04b1a8f1e4d6f3ddd9c07007d4f297fb880
Ektakathuria <surenderkathuria3@gmail.com>
2019-07-10 11:25:34 +0530

    =init

---
+++ src/app.js
@@ -0,0 +1,80 @@
+const express=require('express')
+const forecast=require('./utilis/forecast.js')
+const geocode=require('./utilis/geocode.js')
+
+const app=express()
+const path=require('path')
+const hbs=require('hbs')
+const partialpath=path.join(__dirname,'../templates/partials')
+hbs.registerPartials(partialpath)
+const viewpath=path.join(__dirname,'../templates/views')
+app.set('views',viewpath)
+app.set('view engine','hbs')
+const publicpath=path.join(__dirname,'../public')
+app.use(express.static(publicpath))
+app.get('',(req,res)=>{
+res.render('index',{
+    'title':"Weather",
+    'name':"Ekta Kathuria"
+})
+})
+app.get('/about',(req,res)=>
+{
+res.render('about',{title:"About Page",
+name  : "Sarika Khatri"
+
+})
+})
+app.get('/help',(req,res)=>{
+res.render('help',{
+    title:'Help Page',
+    name: "Megha Chawla"
+})
+})
+app.get('/weather',(req,res)=>{
+    if(!req.query.address)
+    {
+      return   res.render('helpextra',{
+            title:"Address Error",
+            error: "Please Provide the loaction Dear"
+        })
+    }geocode(req.query.address,(error,{location,latitude,longitude}={})=>{
+        
+        if(error)
+        {
+           return  res.send({error
+        })}
+        forecast(latitude,longitude,(error,forecastdata)=>{
+            if(error)
+            {
+           return    res.send(error)
+            }res.send({
+                location,
+                forecast:forecastdata,
+                Address:req.query.address
+            })
+        })
+    })
+   
+})
+app.get('/help/*',(req,res)=>{
+    res.render('helpextra',{
+        title:'404 Help',
+        error:"help Page not found",
+        name:"Latika Sardana"
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('helpextra',{
+        title:'404',
+        error:"404 error",
+        name:"Latika Sardana"
+    })
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is on port 3000")
+
+})
